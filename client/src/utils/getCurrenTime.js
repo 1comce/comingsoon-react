1510cfdce5ddbc90574b56949943a0e797961038
1comce <mainvox9423@gmail.com>
2022-10-30 16:19:42 +0700

    remove file

--- client/src/utils/getCurrenTime.js
+++
@@ -1,8 +0,0 @@
-export function getCurrentTime(separator = '') {
-    let newDate = new Date();
-    let hour = newDate.getHours();
-    let minute = newDate.getMinutes();
-    let second = newDate.getSeconds();
-
-    return `${hour}${separator}${minute < 10 ? `0${minute}` : `${minute}`}${separator}${second}`;
-}
